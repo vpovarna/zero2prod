cf1d341c379df815d4e3317d2cd6a9dc3cf766ac
povarna <vlad.povarna@uipath.com>
2024-08-31 12:28:21 +0300

    Remove comments

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,17 +2,16 @@ use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
-    // Arrange
     let address = spawn_app();
 
     let client = reqwest::Client::new();
-    // Act
+
     let response = client
         .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
-    // Assert
+
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
