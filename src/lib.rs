8770a2d907276537a20658c9da47439c05bd4b48
povarna <vlad.povarna@uipath.com>
2024-08-31 12:22:56 +0300

    Adding random port

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::net::TcpListener;
 use actix_web::{App, HttpResponse, HttpServer, web};
 use actix_web::dev::Server;
 
@@ -6,12 +7,12 @@ async fn health_check() -> HttpResponse {
 }
 
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
     })
-        .bind("127.0.0.1:18000")?
+        .listen(listener)?
         .run();
 
     Ok(server)
